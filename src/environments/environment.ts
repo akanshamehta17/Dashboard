ee3a56ec27de32f7b22a28ff781b1aaa83dde569
Akansha Mehta <akansha.mehta@sjsu.edu>
2018-08-01 12:35:10 -0700

    Update environment.ts

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -5,12 +5,12 @@
 export const environment = {
   production: false,
   firebase: {
-  	apiKey: "AIzaSyAu35Cz9hmyKCm0PIYkTGrEJlK5-ytaKYU",
-    authDomain: "bootstrapangular-207622.firebaseapp.com",
-    databaseURL: "https://bootstrapangular-207622.firebaseio.com",
-    projectId: "bootstrapangular-207622",
-    storageBucket: "bootstrapangular-207622.appspot.com",
-    messagingSenderId: "641233866141"
+  	apiKey: "",
+    authDomain: "",
+    databaseURL: "",
+    projectId: "",
+    storageBucket: "",
+    messagingSenderId: ""
   }
 };
 
